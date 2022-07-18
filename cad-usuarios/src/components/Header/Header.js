7438d6b9e15fe852a0b1802c94b3a832a87068e5
Giovanni Ribeiro Pinto <giovanni.ribeiro@MacBook-Air-de-Giovanni.local>
2022-07-18 19:16:28 -0300

    feat: adicionando projeto

---
+++ cad-usuarios/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './header.css';
+import Logo  from './user.png';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1><img src={Logo} alt='Logo' width='80' /></h1>
+      </header>
+    )
+  }
+}
