5b52cb62aab3bf3f2a5b57b7e51bbe463b77887f
Giovanni Ribeiro Pinto <giovanni.ribeiro@MacBook-Air-de-Giovanni.local>
2022-07-20 12:02:31 -0300

    adicionando cadastro

--- cad-usuarios/src/Pages/CadastroUsers.js
+++ cad-usuarios/src/Pages/CadastroUsers.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./css/cadastro.css";
 
 export default class CadastroUsers extends Component {
   render() {
     return (
-      <div>CadastroUsers</div>
-    )
+      <div className="container-cadUser">
+        <h1 className="cadastro-usuario">Cadastro de usuários</h1>
+        <form>
+          <div className="row">
+            <div className="col">
+              <label>Nome</label>
+              <input type="text" name="nome" value="nome" />
+            </div>
+            <div className="col">
+              <label>Sobre Nome</label>
+              <input type="text" name="sobrenome" value="sobrenome" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label>Email</label>
+              <input type="text" name="email" value="email" />
+            </div>
+          </div>
+          <button>Adicionar</button>
+        </form>
+      </div>
+    );
   }
 }
