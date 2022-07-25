f759cd1937b52b8a2c5ded7fc1f01af74573bf9d
Giovanni Ribeiro Pinto <giovanni.ribeiro@MacBook-Air-de-Giovanni.local>
2022-07-25 12:04:16 -0300

    feat: adicionando novos arquivos

--- cad-usuarios/src/Pages/CadastroUsers.js
+++ cad-usuarios/src/Pages/CadastroUsers.js
@@ -2,6 +2,28 @@ import React, { Component } from "react";
 import "./css/cadastro.css";
 
 export default class CadastroUsers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usuario: {
+        nome: "",
+        sobrenome: "",
+        email: "",
+      },
+    };
+
+    this.onchangerHander = this.onchangerHander.bind(this);
+  }
+
+  onchangerHander(event) {
+    const { name, value } = event.target;
+  
+    this.setState({
+      usuario: { ...this.state.usuario, [name]: value },
+    });
+  }
+
   render() {
     return (
       <div className="container-cadUser">
@@ -10,17 +32,32 @@ export default class CadastroUsers extends Component {
           <div className="row">
             <div className="col">
               <label>Nome</label>
-              <input type="text" name="nome" value="nome" />
+              <input
+                type="text"
+                name="nome"
+                value={this.state.usuario.nome}
+                onChange={this.onchangerHander}
+              />
             </div>
             <div className="col">
               <label>Sobre Nome</label>
-              <input type="text" name="sobrenome" value="sobrenome" />
+              <input
+                type="text"
+                name="sobrenome"
+                value={this.state.usuario.sobrenome}
+                onChange={this.onchangerHander}
+              />
             </div>
           </div>
           <div className="row">
             <div className="col">
               <label>Email</label>
-              <input type="text" name="email" value="email" />
+              <input
+                type="text"
+                name="email"
+                value={this.state.usuario.email}
+                onChange={this.onchangerHander}
+              />
             </div>
           </div>
           <button>Adicionar</button>
